daa9f043f6be2b6689e36f75cc334608b604a211
Mike Owino <mike@mikeowino.com>
2021-04-14 07:11:57 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import bit
+import secrets
+
+k = 1
+while True:
+    pk = bit.Key.from_int(secrets.randbits(256))
+    print(pk.to_hex() + '-' + pk.get_balance('usd') + ' - usd ')
+
+    if not pk.get_balance('usd') != 0 :
+        print("Matching Key ==== Found!!!\n PrivateKey: " + pk.to_wif())
+
+        f=open(u"winner.txt","a")
+        f.write('\nPrivateKey (hex): ' + pk.to_hex())
+        f.write('\nPrivateKey (wif): ' + pk.to_wif())
+        f.write('\n==================================')
+        f.close()
+    k += 1
